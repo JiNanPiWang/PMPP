578e4063de02ebaaef85980e0318d82186fec619
tensor_wsl2 <purep@qq.com>
2025-11-29 15:49:09 +0800

    第三章性能分析

---
+++ 03/profile_matrix_mul.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+nvcc -o matrix_mul matrix_mul_exercise.cu -lineinfo
+
+# -o: 指定输出文件名
+# -f: 强制覆盖旧文件
+# --set full: 跑全量分析 (虽然慢一点，但为了学习一次到位吧！)
+ncu -o matrix_mul_ncu_report -f --set full ./matrix_mul
+
+# -o: 指定输出文件名
+# --stats=true: 在终端直接打印简单的统计信息（可选）
+nsys profile -o matrix_mul_report --stats=true ./matrix_mul
